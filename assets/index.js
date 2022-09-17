33582c5eb485ac1965b605cde47574890544ff36
-v <julio.sergio2709@gmail.com>
2022-09-17 01:31:10 -0400

    update: scripts to modules type

--- assets/index.js
+++ assets/index.js
@@ -1,25 +1,7 @@
+import MediaPlayer from "./MediaPlayer.js";
+
 const video = document.querySelector('video');
 const button = document.querySelector('button')
-
-class MediaPlayer {
-    constructor(config) {
-        this.media = config.el;
-    }
-    play() {
-        this.media.play();
-    }
-    pause() {
-        this.media.pause();
-    }
-    togglePlay() {
-        if (this.media.paused) {
-            this.play();
-        } else {
-            this.pause();
-        }
-    }
-}
-
 
 const player = new MediaPlayer({el: video});
 button.onclick = () => player.togglePlay();
