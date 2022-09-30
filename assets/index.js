98449f36b9f93320bcb0381323fe56a316863068
-v <julio.sergio2709@gmail.com>
2022-09-29 20:58:15 -0400

    update: levenshtein module

--- assets/index.js
+++ assets/index.js
@@ -2,8 +2,8 @@ import MediaPlayer from "./MediaPlayer.js";
 import AutoPlay from "./plugins/AutoPlay.js";
 
 const video = document.querySelector('.movie');
-const playButton = document.querySelector('.play-btn'); 
-const muteButton = document.querySelector('.mute-btn');
+const playButton = document.querySelector('.play_btn'); 
+const muteButton = document.querySelector('.mute_btn');
 
 const player = new MediaPlayer({
     video, 
@@ -14,5 +14,40 @@ const player = new MediaPlayer({
 playButton.onclick = () => player.togglePlay();
 muteButton.onclick = () => player.toggleMute();
 
-var distance = window.Levenshtein.get('back', 'book');
-console.log(distance);
+// text_input-container
+const textInput = document.querySelector('.text_input'); 
+const textInputBtn = document.querySelector('.text_input-btn'); 
+const textOutput = document.querySelector('.text_input-output');
+
+const target = {
+    red: 'Rojo',
+    green: 'Verde',
+    blue: 'Azul'
+}
+const handler = {
+    get(obj, prop) {
+        if (prop in obj) {
+            return obj[prop];
+        }
+        const suggestion = Object.keys(obj).reduce((a, b ) => {
+            return Levenshtein.get(a, prop) < Levenshtein.get(b, prop) ? a : b;
+        });
+
+        if (suggestion) {
+            return suggestion;
+        }
+
+        return false;
+    }
+}
+
+const proxy = new Proxy(target, handler);
+
+textInputBtn.addEventListener('click', () => {
+    const result = proxy[textInput.value];
+    if (result) {
+        textOutput.innerHTML = `no se encontró. Quisiste decir ${result}`;        
+    } else {
+        textOutput.innerHTML = `no se encontró`;  
+    }
+})
