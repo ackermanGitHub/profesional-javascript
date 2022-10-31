a515a126493e6b2932936ba494de540207ee9380
-v <julio.sergio2709@gmail.com>
2022-10-30 21:06:55 -0400

    adding ejercicios to proyect

---
+++ ejercicios/typescript/index.ts
@@ -0,0 +1,15 @@
+// Boolean
+let muted: boolean = true;
+muted = false;
+// muted = "shut-up"; // generates an error 
+
+// numbers
+let numerator: number = 43;
+let denominator: number = 6;
+let result = numerator / denominator;
+
+// Strings
+let nombre: string = 'Richard';
+let saludo = `Me llamo ${nombre}`;
+
+// Arrays
