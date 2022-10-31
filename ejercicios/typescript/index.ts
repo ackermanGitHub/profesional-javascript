4f002d93eb14cb092a5fc62ce949a0e6113c819a
-v <julio.sergio2709@gmail.com>
2022-10-30 21:24:09 -0400

    Basic types variables in TS

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -13,3 +13,28 @@ let nombre: string = 'Richard';
 let saludo = `Me llamo ${nombre}`;
 
 // Arrays
+let people: string[] = [];
+people = ["Isabel", "Nicole", "Raul"];
+people.push("9000");
+// people.push(9000); // generates an error 
+
+let peopleAndNumbers: Array< string | number > = [];
+peopleAndNumbers.push("9000");
+peopleAndNumbers.push(9000);
+
+// Enum
+enum Color {
+    Rojo = "Rojo", 
+    Verde = "Verde",
+    Azul = "Azul",
+}
+
+let colorFavorito: Color = Color.Azul;
+console.log(`Mi color favorito es: ${colorFavorito}`);
+
+// Any
+let comodin: any = "Joker";
+comodin = { type: "WildCard" };
+
+// Object
+let someObject: object = { type: "WildCard" };
